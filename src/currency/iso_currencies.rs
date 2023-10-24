4ca9503802b596787bcdc1518efd8b7fe848ae32
David <davidj.geib@gmail.com>
2023-10-24 11:30:35 +0200

    x

--- src/currency/iso_currencies.rs
+++ src/currency/iso_currencies.rs
@@ -4,7 +4,7 @@ pub mod iso {
     use std::fmt;
 
     /// Represents a single ISO-4217 currency (e.g. USD).
-    #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, Hash)]
+    #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
     pub struct Currency {
         pub iso_alpha_code: &'static str,
         pub exponent: u32,
